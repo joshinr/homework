7df1cadbd217197a8683b91d660e51e3b66388c1
joshinr <joshinr@gmail.com>
2018-01-04 20:26:17 -0600

    Add files via upload

---
+++ max_heap.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class MaxHeap 
+{
+private:
+    vector<int> arr;
+
+    int Left(int i)
+    {
+        return 2*i + 1;
+    }
+
+    int Right(int i)
+    {
+        return 2*i + 2;
+    }
+
+    int Parent(int i)
+    {
+        return (i-1)/2;
+    }
+
+    void HeapifyUp(int i)
+    {
+        if (i <= 0)
+        {
+            return;
+        }
+
+        int parent = Parent(i);
+
+        if (arr[parent] < arr[i])
+        {
+            swap(arr[parent], arr[i]);
+            HeapifyUp(parent);
+        }
+    }
+    
+    void HeapifyDown(int i)
+    {
+        int left = Left(i);
+        int right = Right(i);
+        int largest = i;
+
+        if (left < Size() && arr[left] > arr[i])
+        {
+            largest = left;
+        }
+
+        if (right < Size() && arr[right] > arr[i])
+        {
+            largest = right;
+        }
+
+        if (largest != i)
+        {
+            swap(arr[largest], arr[i]);
+            HeapifyDown(largest);
+        }
+    }
+
+public:
+
+    void Push(int i)
+    {
+        arr.push_back(i);
+        HeapifyUp(Size() - 1);
+    }
+
+    void Pop()
+    {
+        if (arr.empty())
+        {
+            throw out_of_range("out of range");
+        }
+
+        int ret = arr[0];
+        arr[0] = arr.back();
+        arr.pop_back();
+        HeapifyDown(0);
+    }
+
+    int Top()
+    {
+        if (arr.empty())
+        {   
+            throw out_of_range("out of range");
+        }
+
+        return arr[0];
+    }   
+
+    int Size()
+    {
+        return arr.size();
+    }   
+};
+
+int main(int argc, char **argv)
+{
+    MaxHeap mp;
+
+    cout<<"Pushing 3 2 15"<<endl;
+    mp.Push(7);
+    mp.Push(8);
+    mp.Push(15);
+       
+    cout<<"Size is "<<mp.Size()<<endl;
+    
+    cout<<mp.Top()<<endl;
+    mp.Pop();
+
+    cout<<mp.Top()<<endl;
+    mp.Pop();
+
+    cout<<mp.Top()<<endl;
+
+    cout<<"Pushing 5 4 45"<<endl;
+
+    mp.Push(5);
+    mp.Push(4);
+    mp.Push(45);
+
+    cout<<"Size is "<<mp.Size()<<endl;
+
+    cout<<mp.Top()<<endl;
+    mp.Pop();
+
+    cout<<mp.Top()<<endl;
+    mp.Pop(); 
+
+    cout<<mp.Top()<<endl;
+    return 0;
+}
